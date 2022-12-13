2083968fe367cc61a17a5e97886a71ec02f8625a
Dann1140 <danielcan.16@gmail.com>
2022-12-13 16:03:04 -0500

    Made basic responsive design

--- src/todoCounter/index.js
+++ src/todoCounter/index.js
@@ -6,7 +6,7 @@ function TodoCounter() {
   const { totalTodos, completedTodos } = React.useContext(TodoContext);
   return (
     <h2 className="todoCounter">
-      Has completado {completedTodos} de {totalTodos} Tareas
+      Has completado {completedTodos} de {totalTodos} Tarea(s)
     </h2>
   );
 }
